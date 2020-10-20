5f681cba8fb1d1fdec86902e2a8cb7cc86ccadb9
Aliaksandr Kulik <inter_net@tut.by>
2020-10-21 02:25:53 +0300

    feat: add paginator for 1080p

--- shelter/assets/js/pets.js
+++ shelter/assets/js/pets.js
@@ -3,15 +3,25 @@ let burger = document.querySelector(".burger"),
 	burger_menu_links = document.querySelectorAll(".nav li"),
 	body = document.querySelector("body"),
 	mask = document.querySelector(".mask"),
-	// arrowL = document.querySelector(".arrowL"),
-	// arrowR = document.querySelector(".arrowR"),
-	// slider = document.querySelector(".friends__slider_viewport"),
-	// slides = document.querySelectorAll(".slide"),
-	popup = document.querySelector(".popup");
+	firstPageBtn = document.querySelector(".firstPage"),
+	arrowL = document.querySelector(".arrowL"),
+	arrowR = document.querySelector(".arrowR"),
+	currentPageBtn = document.querySelector(".currentPage"),
+	lastPageBtn = document.querySelector(".lastPage"),
+	slider = document.querySelector(".pets-page__content_pets"),
+	slides = document.querySelectorAll(".pets__tile"),
+	popup = document.querySelector(".popup"),
+	pets = [];
 
-	for (let i = 0; i < burger_menu_links.length; i++) {
-		burger_menu_links[i].addEventListener('click', e => e.stopPropagation());
-	}
+//------------------------------------------------------cloning JSON in pets = [] ----------
+	
+	fetch('../../assets/js/pets.json').then((response) => {
+		response.json().then((text) => {
+		pets = JSON.parse(JSON.stringify(text));
+		})
+	}) 
+
+//----------------------------------------------event close mask + menu if rotate mobile device & width > 767
 
 window.addEventListener('resize', function(event){
   if (window.innerWidth > 767) {
@@ -22,6 +32,7 @@ window.addEventListener('resize', function(event){
 	//popup.classList.remove("active");
   }
 });
+//-----------------------------------------------events for burger/menu & slide up-----------------
 
 burger.addEventListener("click", function () {
 	this.classList.toggle("active");
@@ -29,6 +40,7 @@ burger.addEventListener("click", function () {
 	burger_menu.classList.toggle("active");
 	body.classList.toggle("active");
 })
+
 burger_menu.addEventListener("click", function () {
 	this.classList.toggle("active");
 	mask.classList.toggle("active");
@@ -36,6 +48,7 @@ burger_menu.addEventListener("click", function () {
 	body.classList.toggle("active");
 	
 })
+
 mask.addEventListener("click", function () {
 	this.classList.remove("active");
 	burger_menu.classList.remove("active");
@@ -45,7 +58,6 @@ mask.addEventListener("click", function () {
 	
 })
 
-
 document.querySelector(".nav_current").addEventListener("click", function () {
 	burger_menu.classList.remove("active");
 	mask.classList.remove("active");
@@ -53,6 +65,14 @@ document.querySelector(".nav_current").addEventListener("click", function () {
 	burger.classList.remove("active");
 	body.scrollTo({ top: 0, behavior: 'smooth' });
 })
+
+//----------------------------------------------remove event from menu area---------------------------
+
+for (let i = 0; i < burger_menu_links.length; i++) { 
+		burger_menu_links[i].addEventListener('click', e => e.stopPropagation());
+	}
+
+//---------------------------------------------------------------------------animation
 
  // function reset_animation() { //сюда сразу запихнул рандомизацию слайдов
 	// setTimeout(function (){
@@ -77,12 +97,85 @@ document.querySelector(".nav_current").addEventListener("click", function () {
 // 		reset_animation()
 // 	}
 // })
+//------------------------------------------------------ functions get random number inclusive interval
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min; 
+}
 
-// function getRandomIntInclusive(min, max) {
-//   // min = Math.ceil(min);
-//   // max = Math.floor(max);
-//   return Math.floor(Math.random() * (max - min + 1)) + min; 
-// }
+
+let arrRandomNum = []; // нужно обнулить, если перезапускаем функцию
+let multiRandom = [[4, 0, 2, 1, 5, 7, 3, 6]]; //тут будет формироваться массив 6*8 при загрузке страницы;
+function getRandEigth (qty) {
+	if (arrRandomNum.length < qty) {
+		let number = getRandomIntInclusive(0, 7); // заданный интервал 0-7 - это индексы массива pets из 8 элемента
+		if (arrRandomNum.includes(number)) {
+			getRandEigth(qty)
+		} else {
+			arrRandomNum.push(number);
+			getRandEigth(qty)}
+	} else { return }
+}
+
+//формируем массив из 48 элементов, мне нужен  только многомерный массив с индексами объекта pet
+//но так как PP то я первый элемент из 8 цифр добавил как в макете
+for (let i = 1; i < 6; i++) {
+	getRandEigth(8)
+	multiRandom.push(arrRandomNum);
+	arrRandomNum = [];
+}
+
+function pagination(pageNumber) {
+	for (let i = 0; i < slides.length; i++) {
+		slides[i].childNodes[3].textContent = pets[multiRandom[pageNumber][i]].name;
+		slides[i].childNodes[1].alt = pets[multiRandom[pageNumber][i]].name + " picture";
+		slides[i].childNodes[1].src = pets[multiRandom[pageNumber][i]].img;
+	}
+}
+
+arrowR.addEventListener("click", function () {
+	pagination(currentPageBtn.innerHTML);
+	currentPageBtn.innerHTML++;
+	if (+currentPageBtn.innerHTML === 6) {
+		lastPageBtn.disabled = true;
+		arrowR.disabled = true;
+	} else if (arrowL.disabled === true) {
+		firstPageBtn.disabled = false;
+		arrowL.disabled = false;
+	}
+})
+
+
+arrowL.addEventListener("click", function () {
+	pagination(currentPageBtn.innerHTML - 2);
+	currentPageBtn.innerHTML--;
+	if (+currentPageBtn.innerHTML === 1) {
+		firstPageBtn.disabled = true;
+		arrowL.disabled = true;
+	} else if (arrowR.disabled === true) {
+		lastPageBtn.disabled = false;
+		arrowR.disabled = false;
+	}
+})
+
+
+lastPageBtn.addEventListener("click", function () {
+	pagination(5);
+	currentPageBtn.innerHTML = 6;
+		lastPageBtn.disabled = true;
+		arrowR.disabled = true;
+		firstPageBtn.disabled = false;
+		arrowL.disabled = false;
+})
+
+firstPageBtn.addEventListener("click", function () {
+	pagination(0);
+	currentPageBtn.innerHTML = 1;
+		lastPageBtn.disabled = false;
+		arrowR.disabled = false;
+		firstPageBtn.disabled = true;
+		arrowL.disabled = true;
+})
+
 // function randomSlide () {
 // 	let first = getRandomIntInclusive(0, 7);
 // 	let second = getRandomIntInclusive(0, 7);
@@ -116,20 +209,20 @@ for (let i = 0; i < slides.length; i++) {
 	})
 }
 
-// function popup_key (i) {
-// 	key = slides[i].childNodes[3].textContent;
-// 	index = pets.findIndex(j => j.name === key);
-// 	popup.childNodes[3].src = pets[index].img;
-// 	popup.childNodes[5].childNodes[1].textContent = key;
-// 	popup.childNodes[5].childNodes[3].textContent = `${pets[index].type} - ${pets[index].breed}`;
-// 	popup.childNodes[5].childNodes[5].childNodes[0].textContent = pets[index].description;
-// 	popup.childNodes[5].childNodes[7].childNodes[1].childNodes[1].textContent = pets[index].age;
-// 	popup.childNodes[5].childNodes[7].childNodes[3].childNodes[1].textContent = pets[index].inoculations;
-// 	popup.childNodes[5].childNodes[7].childNodes[5].childNodes[1].textContent = pets[index].diseases;
-// 	popup.childNodes[5].childNodes[7].childNodes[7].childNodes[1].textContent = pets[index].parasites;
+function popup_key (i) {
+	key = slides[i].childNodes[3].textContent;
+	index = pets.findIndex(j => j.name === key);
+	popup.childNodes[3].src = pets[index].img;
+	popup.childNodes[5].childNodes[1].textContent = key;
+	popup.childNodes[5].childNodes[3].textContent = `${pets[index].type} - ${pets[index].breed}`;
+	popup.childNodes[5].childNodes[5].childNodes[0].textContent = pets[index].description;
+	popup.childNodes[5].childNodes[7].childNodes[1].childNodes[1].textContent = pets[index].age;
+	popup.childNodes[5].childNodes[7].childNodes[3].childNodes[1].textContent = pets[index].inoculations;
+	popup.childNodes[5].childNodes[7].childNodes[5].childNodes[1].textContent = pets[index].diseases;
+	popup.childNodes[5].childNodes[7].childNodes[7].childNodes[1].textContent = pets[index].parasites;
 
 
-// }
+}
 
 document.querySelector(".popup_close").addEventListener("click", function () {
 	document.querySelector(".popup").classList.remove("active");
