c20bc91d5a02060587a4338dc98d2574d8c5eda7
Aliaksandr Kulik <inter_net@tut.by>
2020-10-22 21:07:19 +0300

    refactor: change pathes

--- shelter/pages/main/main.js
+++ shelter/pages/main/main.js
@@ -1,47 +1,22 @@
-// document.querySelector(".help_link").addEventListener("click", function () {
-// 	this.classList.add("active");
-// 	setTimeout(function (){
-// 		document.querySelector(".help_link").innerHTML = "Under<br>construction";
-// 		document.querySelector(".help_link").style.cssText = "width: 105.11px"; 
-// 	}, 1000)
-// 	setTimeout(function (){
-// 		document.querySelector(".help_link").innerHTML = "Help the shelter";
-// 	}, 3000)
-
-// 	setTimeout(function (){
-// 		document.querySelector(".help_link").classList.remove("active");
-// 	}, 4000)
-// })
-
-// document.querySelector(".contacts_link").addEventListener("click", function () {
-// 	this.classList.add("active");
-// 	setTimeout(function (){
-
-// 		document.querySelector(".contacts_link").innerHTML = "Under<br>construction";
-// 	}, 1000)
-// 	setTimeout(function (){
-// 		document.querySelector(".contacts_link").innerHTML = "Contacts";
-// 	}, 3000)
-
-// 	setTimeout(function (){
-// 		document.querySelector(".contacts_link").classList.remove("active");
-// 	}, 4000)
-// })
-
 let burger = document.querySelector(".burger"),
 	burger_menu = document.querySelector(".nav"),
-	burger_menu_links = document.querySelectorAll(".nav li"),
+	burger_menu_links = document.querySelector(".nav ul"),
 	body = document.querySelector("body"),
 	mask = document.querySelector(".mask"),
 	arrowL = document.querySelector(".arrowL"),
 	arrowR = document.querySelector(".arrowR"),
 	slider = document.querySelector(".friends__slider_viewport"),
 	slides = document.querySelectorAll(".slide"),
+	arrRandomNum = [], // нужно обнулить, если перезапускаем функцию
+	repeat = [4, 0, 2],
 	popup = document.querySelector(".popup");
 
-	for (let i = 0; i < burger_menu_links.length; i++) {
-		burger_menu_links[i].addEventListener('click', e => e.stopPropagation());
-	}
+	pets = JSON.parse(JSON.stringify(jsonPets));
+
+	// for (let i = 0; i < burger_menu_links.length; i++) {
+	// 	burger_menu_links[i].addEventListener('click', e => e.stopPropagation());
+	// }
+	burger_menu_links.addEventListener('click', e => e.stopPropagation())
 
 window.addEventListener('resize', function(event){
   if (window.innerWidth > 767) {
@@ -49,7 +24,7 @@ window.addEventListener('resize', function(event){
 	burger_menu.classList.remove("active");
 	burger.classList.remove("active");
 	body.classList.remove("active");
-	//popup.classList.remove("active");
+	popup.classList.remove("active");
   }
 });
 
@@ -60,10 +35,11 @@ burger.addEventListener("click", function () {
 	body.classList.toggle("active");
 })
 burger_menu.addEventListener("click", function () {
-	this.classList.toggle("active");
-	mask.classList.toggle("active");
-	burger.classList.toggle("active");
-	body.classList.toggle("active");
+	this.classList.remove("active");
+	mask.classList.remove("active");
+	burger.classList.remove("active");
+	body.classList.remove("active");
+	console.log("asd")
 	
 })
 mask.addEventListener("click", function () {
@@ -109,29 +85,47 @@ arrowR.addEventListener("click", function () {
 })
 
 function getRandomIntInclusive(min, max) {
-  // min = Math.ceil(min);
-  // max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; 
 }
+
+function getRandEigth (qty) {
+	if (arrRandomNum.length < qty) {
+		let number = getRandomIntInclusive(0, 7); // заданный интервал 0-7 - это индексы массива pets из 8 элемента
+		if (arrRandomNum.includes(number)) {
+			getRandEigth(qty)
+		} else {
+			arrRandomNum.push(number);
+			getRandEigth(qty)}
+	} else { return }
+}
+
 function randomSlide () {
-	let first = getRandomIntInclusive(0, 7);
-	let second = getRandomIntInclusive(0, 7);
-	if (second === first) {
-		second = getRandomIntInclusive(0, 7)
-	}
-	let third = getRandomIntInclusive(0, 7);
-	if (third === first || third === second) {
-		third = getRandomIntInclusive(0, 7)
-	}
-	slides[0].childNodes[3].textContent = pets[first].name;
-	slides[0].childNodes[1].alt = pets[first].name + " picture";
-	slides[0].childNodes[1].src = pets[first].img;
-	slides[1].childNodes[3].textContent = pets[second].name;
-	slides[1].childNodes[1].alt = pets[second].name + " picture";
-	slides[1].childNodes[1].src = pets[second].img;
-	slides[2].childNodes[3].textContent = pets[third].name;
-	slides[2].childNodes[1].alt = pets[third].name + " picture";
-	slides[2].childNodes[1].src = pets[third].img;
+	getRandEigth (3);
+	if (repeat.includes(arrRandomNum[0])) {
+			arrRandomNum = [];
+			randomSlide ()
+		}
+		else if (repeat.includes(arrRandomNum[1])) {
+			arrRandomNum = [];
+			randomSlide ()
+		}
+		else if (repeat.includes(arrRandomNum[2])) {
+			arrRandomNum = [];
+			randomSlide ()
+		} else {
+				slides[0].childNodes[3].textContent = pets[arrRandomNum[0]].name;
+				slides[0].childNodes[1].alt = pets[arrRandomNum[0]].name + " picture";
+				slides[0].childNodes[1].src = pets[arrRandomNum[0]].img;
+				slides[1].childNodes[3].textContent = pets[arrRandomNum[1]].name;
+				slides[1].childNodes[1].alt = pets[arrRandomNum[1]].name + " picture";
+				slides[1].childNodes[1].src = pets[arrRandomNum[1]].img;
+				slides[2].childNodes[3].textContent = pets[arrRandomNum[2]].name;
+				slides[2].childNodes[1].alt = pets[arrRandomNum[2]].name + " picture";
+				slides[2].childNodes[1].src = pets[arrRandomNum[2]].img;
+				repeat = [];
+				repeat = [...arrRandomNum]; //для неповтора, когда слайд 1
+				arrRandomNum = [];
+			}
 }
 
 //-------------------------------------modals popups-------------------------
